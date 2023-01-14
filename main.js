f9336899eecd16feed2dc3149badce007cf77322
kize <bochu.nicolas@hotmail.fr>
2023-01-14 20:15:03 +0100

    add random team

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const poke_container = document.getElementById('poke_container');
+const pokerand_container = document.getElementById('pokerand_container');
+let isEmpty = document.getElementById('pokerand_container').innerHTML === "";
 const pokemons_number = 151;
+const pokemons_team = 6;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -18,20 +21,34 @@ const colors = {
 };
 
 const main_types = Object.keys(colors);
-console.log(main_types)
 const fetchPokemons = async () => {
     for (let i=1; i<=pokemons_number; i++) {
         await getPokemon(i);
     }
 }
 
-
+const randomTeam = async () => {
+    if (!isEmpty) {
+        document.getElementById("pokerand_container").innerHTML = "";
+    }
+    for (let i=1; i<=pokemons_team; i++) {
+        let rand = Math.floor(Math.random() * 151);
+        await getPokemonTeam(rand)
+    }
+}
 
 const getPokemon = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
     createPokemonCard(pokemon);
+}
+
+const getPokemonTeam = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonCardTeam(pokemon);
 }
 
 fetchPokemons();
@@ -58,7 +75,33 @@ function createPokemonCard (pokemon) {
         </div>
         
     `;
-
     pokemonEl.innerHTML = pokeInnerHTML;
      poke_container.appendChild(pokemonEl);
+}
+
+function createPokemonCardTeam (pokemon) {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    const poke_types = pokemon.types.map(el => el.type.name);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const img = pokemon.sprites.front_default;
+    const color = colors[type];
+
+    pokemonEl.style.backgroundColor = color;
+
+    const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="${img}" alt="pokemon"/>
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type : <span>${type}</span></small>
+        </div>
+        
+    `;
+    pokemonEl.innerHTML = pokeInnerHTML;
+    pokerand_container.appendChild(pokemonEl);
 }
